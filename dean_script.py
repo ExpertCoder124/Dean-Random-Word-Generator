2c89471d8b6f092267e964d87382d7397718a1c3
ExpertCoder124 <113040894+ExpertCoder124@users.noreply.github.com>
2024-05-28 08:54:20 -0400

    Update dean_script.py

--- dean_script.py
+++ dean_script.py
@@ -1,5 +1,5 @@
 import random
-import re # cuz sting.replace doesnt seem to be working for \n
+
 words = open('words.txt', 'r')
 wordlist = [line.split(',') for line in words.readlines()]
 #turning the words.txt file into a list
@@ -27,5 +27,14 @@ word9 = wordlist[num9]
 word10 = wordlist[num10]
 # assigning variables. It's not good practice to dynamically assign varibles, but I may try dictionary key:value pairs in the future 
 words = str(word1 + word2 + word3 + word4 + word5 + word6 + word7 + word8 + word9 + word10)
-s = words.strip()
-print(s) # s stands for sentence
+
+#parsing string
+words = words.replace(',',' ')
+words = words.replace('_',' ')
+words = words.replace("'",'')
+words = words.replace('[','')
+words = words.replace(']','')
+words = words.replace('\\n','')
+
+print(words) 
+
